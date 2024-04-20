1e1939ee7b58990eb208e3cebadd4c62fcf10d30
Piotr Dubiela <piotrd.tnt@gmail.com>
2024-04-20 22:28:30 +0200

    breaking formatting

--- RestAssuredTests/src/main/java/pl/tnt/tjb/serialization/java/Book.java
+++ RestAssuredTests/src/main/java/pl/tnt/tjb/serialization/java/Book.java
@@ -7,7 +7,7 @@ public class Book implements Serializable {
     private final String title;
     private final String author;
 
-    public Book(String title, String author) {
+    public Book   (String title, String author) {
         this.title = title;
         this.author = author;
     }
@@ -16,7 +16,7 @@ public class Book implements Serializable {
         return title;
     }
 
-    public String getAuthor() {
+    public String getAuthor     ()    {
         return author;
     }
 
