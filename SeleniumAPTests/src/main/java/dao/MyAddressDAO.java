3f565ca2763e7ae7882d49fe87a83e1eec2aa3ca
Piotr Dubiela <piotrd.tnt@gmail.com>
2025-05-17 17:27:23 +0200

    Added builder pattern

--- SeleniumAPTests/src/main/java/dao/MyAddressDAO.java
+++ SeleniumAPTests/src/main/java/dao/MyAddressDAO.java
@@ -76,4 +76,84 @@ public class MyAddressDAO {
     public String getAddressAliasName() {
         return addressAliasName;
     }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String company;
+        private String addressLine;
+        private String addressLine2;
+        private String city;
+        private String state;
+        private String zipPostalCode;
+        private String homePhone;
+        private String mobilePhone;
+        private String additionalInfo;
+        private String addressAliasName;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setCompany(String company) {
+            this.company = company;
+            return this;
+        }
+
+        public Builder setAddressLine(String addressLine) {
+            this.addressLine = addressLine;
+            return this;
+        }
+
+        public Builder setAddressLine2(String addressLine2) {
+            this.addressLine2 = addressLine2;
+            return this;
+        }
+
+        public Builder setCity(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder setState(String state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder setZipPostalCode(String zipPostalCode) {
+            this.zipPostalCode = zipPostalCode;
+            return this;
+        }
+
+        public Builder setHomePhone(String homePhone) {
+            this.homePhone = homePhone;
+            return this;
+        }
+
+        public Builder setMobilePhone(String mobilePhone) {
+            this.mobilePhone = mobilePhone;
+            return this;
+        }
+
+        public Builder setAdditionalInfo(String additionalInfo) {
+            this.additionalInfo = additionalInfo;
+            return this;
+        }
+
+        public Builder setAddressAliasName(String addressAliasName) {
+            this.addressAliasName = addressAliasName;
+            return this;
+        }
+
+        public MyAddressDAO build() {
+            return new MyAddressDAO(firstName, lastName, company, addressLine, addressLine2, city, state, zipPostalCode,
+                    homePhone, mobilePhone, additionalInfo, addressAliasName);
+        }
+    }
 }
--- SeleniumAPTests/src/test/java/test/AddressTest.java
+++ SeleniumAPTests/src/test/java/test/AddressTest.java
@@ -30,9 +30,17 @@ public class AddressTest extends BaseTest {
 
     @Test
     public void userIsAbleToAddAddress() {
-        MyAddressDAO myAddressDAO = new MyAddressDAO("John", "Smith", "", "WallStreet 1/2", "",
-                "New York", "New York", "11111", "12345678", "", "",
-                ORIGINAL_ADDRESS);
+        MyAddressDAO myAddressDAO = new MyAddressDAO.Builder()
+                .setFirstName("John")
+                .setLastName("Smith")
+                .setAddressLine("WallStreet 1/2")
+                .setAddressLine2("Manhattan")
+                .setCity("New York")
+                .setState("New York")
+                .setZipPostalCode("11111")
+                .setHomePhone("12345678")
+                .setAddressAliasName(ORIGINAL_ADDRESS)
+                .build();
 
         List<String> displayedAddresses = new TopNavigationBar(driver).openUserAccountDetails()
                 .openMyAddresses().openNewAddressAddition()
